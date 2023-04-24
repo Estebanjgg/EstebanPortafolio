e6b76663478d97cb2fa2b79fa1007a67442cfb5e
esteban <esteban030991@gmail.com>
2023-04-23 23:53:01 -0300

    footer update header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import Hero from '../components/Hero/Hero'
-import Skills from '../components/Skills/Skills'
+
 
 export default function Home() {
   return (
     <>
       <Hero/>
-      <Skills/>
+      
     </>
   )
 }
