77d010c4a0f6b9f30829aef838c2b4219f9bc43b
esteban <esteban030991@gmail.com>
2023-04-25 23:44:09 -0300

    ajustando index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import Hero from '../components/Hero/Hero'
-
-
+import Skill from '../components/Skills/Skills'
+import { AboutTemplete } from "../templetes";
+import { ContactTemplete } from "../templetes";
+import { ProjectsTemplete } from "../templetes";
 
 export default function Home() {
   return (
     <>
       <Hero/>
-      
+      <Skill/>
+      <ProjectsTemplete/>
+      <AboutTemplete/>
+      <ContactTemplete/>
+
     </>
   )
 }
