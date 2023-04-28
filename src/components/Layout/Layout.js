3cbcfc61993c087f283b111f7bf51e3277640853
esteban <esteban030991@gmail.com>
2023-04-28 02:59:55 -0300

    theme

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import Navbar from '../NavBar/Navbar';
-import Footer from '../Footer/Footer';
+import React from "react";
+import Navbar from "../NavBar/Navbar";
+import Footer from "../Footer/Footer";
+import { useTheme } from "../ThemeSwitcher/ThemeContext";
 
 const Layout = ({ children }) => {
+  const { currentTheme } = useTheme();
+  
   return (
-    <>
+    <div style={currentTheme}>
       <Navbar />
       <main>{children}</main>
       <Footer />
-    </>
-  )
+    </div>
+  );
 }
-
-export default Layout;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,16 @@
-import Layout from '../components/Layout/Layout';
-import 'swiper/swiper-bundle.min.css';
-import '../styles/globals.css';
-import React from "react";
-import { ThemeProvider } from '../components/ThemeSwitcher/ThemeContext';
+import Layout from "../components/Layout/Layout";
+import "../styles/globals.css";
+import { ThemeProvider } from "../components/ThemeSwitcher/ThemeContext";
+
 
 function MyApp({ Component, pageProps }) {
+  
   return (
-    <Layout>
-      <ThemeProvider>
+    <ThemeProvider>
+      <Layout>
         <Component {...pageProps} />
+      </Layout>
       </ThemeProvider>
-    </Layout>
   );
 }
 
